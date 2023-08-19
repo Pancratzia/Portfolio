14fec38279e141c71032680cc0923962d00d5871
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-08-19 17:28:45 -0400

    Header ready!

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,39 +2,43 @@ import "./Header.scss";
 import { FaSquareTwitter, FaSquareGithub, FaLinkedin } from "react-icons/fa6";
 
 function Header() {
-
-  const title= "< Laura Ortega />";
+  const title = "< Laura Ortega />";
   return (
-    <div className="header">
+    <div className="header" id="header">
       <div className="container">
         <div className="top-header">
-          <a href="/">
-            <h1>
-              {title} <span>Pancratzia </span>
-            </h1>
-          </a>
-          <h2>FullStack Web Developer</h2>
+          <h1>
+            <a href="/">{title}</a> <span>Pancratzia </span>
+          </h1>
+
+          <h2>FullStack Developer</h2>
         </div>
 
         <div className="middle-header">
           <ul className="socials">
             <li>
-              <a href="https://twitter.com/pancratzia"><FaSquareTwitter /></a>
+              <a href="https://twitter.com/pancratzia">
+                <FaSquareTwitter />
+              </a>
             </li>
             <li>
-              <a href="https://www.linkedin.com/in/leortegaperez/"><FaLinkedin /></a>
+              <a href="https://www.linkedin.com/in/leortegaperez/">
+                <FaLinkedin />
+              </a>
             </li>
             <li>
-              <a href="https://github.com/pancratzia"><FaSquareGithub /></a>
+              <a href="https://github.com/pancratzia">
+                <FaSquareGithub />
+              </a>
             </li>
           </ul>
         </div>
 
         <div className="bottom-header">
           <nav className="nav">
-            <ul>
+            <ul className="active">
               <li>
-                <a href="/">Home</a>
+                <a href="#header">Home</a>
               </li>
               <li>
                 <a href="#about">About</a>
