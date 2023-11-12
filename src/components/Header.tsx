31266341534ce9001ba87c2dade29f69901871a4
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-11-12 19:58:26 -0400

    starting with header

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+  return (
+    <div className="header">
+        <h1 className="header__title">&#60; Laura Ortega &#47;&#62;</h1>
+    </div>
+  )
+}
+
+export default Header
