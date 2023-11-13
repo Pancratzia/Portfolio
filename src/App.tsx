fecc8dd9c6ff8b4e3f54fac9a234f51e03430200
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-11-12 23:47:11 -0400

    Finishing with Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <div>
       <Header />
+      Hola mundo
     </div>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,49 @@
 const Navbar = () => {
+  const handleClick = () => {
+    const openNavbar = document.getElementById("openNavbar");
+    const closeNavbar = document.getElementById("closeNavbar");
+    const navbarLinks = document.querySelectorAll(".navbar__link");
+
+    openNavbar?.classList.toggle("navbar__icon--hidden");
+    openNavbar?.classList.toggle("navbar__icon--visible");
+    closeNavbar?.classList.toggle("navbar__icon--visible");
+    closeNavbar?.classList.toggle("navbar__icon--hidden");
+
+    navbarLinks.forEach((link) => {
+      link.classList.toggle("navbar__link--visible");
+      link.classList.toggle("navbar__link--hidden");
+    })
+
+  };
+
   return (
-    <div className="navbar">
-        Navbar
-    </div>
+    <nav className="navbar">
+      <div className="navbar__action" onClick={handleClick}>
+        <i
+          id="openNavbar"
+          className="navbar__icon fas fa-bars navbar__icon--visible"
+        ></i>
+        <i
+          id="closeNavbar"
+          className="navbar__icon fas fa-times navbar__icon--hidden"
+        ></i>
+      </div>
+      <a href="#" className="navbar__link navbar__link--hidden">
+        Home
+      </a>
+      <a href="#" className="navbar__link navbar__link--hidden">
+        About Me
+      </a>
+      <a href="#" className="navbar__link navbar__link--hidden">
+        Skills
+      </a>
+      <a href="#" className="navbar__link navbar__link--hidden">
+        Projects
+      </a>
+      <a href="#" className="navbar__link navbar__link--hidden">
+        Contact
+      </a>
+    </nav>
   );
 };
 
