648976488db4c002dfa9cb8f6a283894572ec373
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-11-13 10:10:30 -0400

    Assing image

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import Header from "./components/Header"
-
+import About from "./components/About";
+import Header from "./components/Header";
 
 function App() {
-
   return (
     <div>
       <Header />
-      Hola mundo
+      <About />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/About.tsx
@@ -0,0 +1,13 @@
+
+
+const About = () => {
+  return (
+    <div className="about">
+        <div className="about__image-container">
+          <img className="about__image" src="/public/img/personal/profile.png" alt="About" />
+        </div>
+    </div>
+  )
+}
+
+export default About;
