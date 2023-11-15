df4dfd34760e944205dc148f090bdfee8eb13c09
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-11-15 10:14:54 -0400

    Creating project component

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,48 +1,100 @@
+interface Props {
+  title: string;
+  image: string;
+  description: string;
+  github: string;
+  demo: string;
+}
+
+export const Project = (props: Props) => {
+  const { title, image, description, github, demo } = props;
+  return (
+    <div className="project">
+      <h3 className="project__title">{title}</h3>
+      <picture>
+        <source srcSet={`img/projects/${image}.avif`} type="image/avif" />
+        <source srcSet={`img/projects/${image}.webp`} type="image/webp" />
+        <source srcSet={`img/projects/${image}.png`} type="image/png" />
+
+        <img
+          className="project__image"
+          src={`img/projects/${image}.png`}
+          width="300"
+          height="100%"
+          alt={`Project: ${title}`}
+          loading="lazy"
+        />
+      </picture>
+      <p className="project__description">{description}</p>
+
+      <div className="project__links">
+        <a
+          className="project__link"
+          href={github}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <i className="fab fa-github-square"></i>
+        </a>
+        <a
+          className="project__link"
+          href={demo}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <i className="fa fa-external-link"></i>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+const PROJECTS = [
+  {
+    title: "Project 1",
+    image: "project1",
+    description: "Project 1 description",
+    github: "https://github.com",
+    demo: "https://github.com", 
+  },
+  {
+    title: "Project 2",
+    image: "project2",
+    description: "Project 2 description",
+    github: "https://github.com",
+    demo: "https://github.com",
+  },
+  {
+    title: "Project 3",
+    image: "project3",
+    description: "Project 3 description",
+    github: "https://github.com",
+    demo: "https://github.com",
+  },
+  {
+    title: "Project 4",
+    image: "project4",
+    description: "Project 4 description",
+    github: "https://github.com",
+    demo: "https://github.com",
+  }
+]
+
 const Projects = () => {
   return (
     <div className="projects">
       <h2 className="projects__heading">My Projects</h2>
       <div className="projects__grid">
-        <div className="project">
-          <h3 className="project__title">My Portfolio</h3>
-          <picture>
-
-            <source srcSet="img/projects/portfolio.avif" type="image/avif" />
-            <source srcSet="img/projects/portfolio.webp" type="image/webp" />
-            <source srcSet="img/projects/portfolio.png" type="image/png" />
-
-            <img 
-              className="project__image"
-              src="img/projects/portfolio.png"
-              width="300"
-              height="100%"
-              alt="Portfolio"
-              loading="lazy"
-            />
-          </picture>
-          <p className="project__description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi consequatur accusantium soluta perspiciatis veniam laboriosam ab maiores ad illo praesentium magni possimus molestias eos corrupti vitae vero dignissimos, molestiae, minus neque inventore. Consectetur asperiores magni at accusantium nihil libero optio. Quod debitis vero quidem exercitationem a sint assumenda ipsa ipsum.
-          </p>
-
-          <div className="project__links">
-            <a
-              className="project__link"
-              href="https://github.com/pancratzia/portfolio"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <i className="fab fa-github-square"></i>
-            </a>
-            <a
-              className="project__link"
-              href="#"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <i className="fa fa-external-link"></i>
-            </a>
-          </div>
-        </div>
+        {PROJECTS.map((project, index) => (
+          <Project
+            key={index}
+            title={project.title}
+            image={project.image}
+            description={project.description}
+            github={project.github}
+            demo={project.demo}
+          />
+        ))}
       </div>
     </div>
   );
